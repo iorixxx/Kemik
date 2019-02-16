ce1ea63d80535d5996d7196cb8d46d60bcb4c191
Ahmet Arslan <iorixxx@yahoo.com>
2019-02-17 02:12:56 +0300

    add props file

--- src/main/java/edu/anadolu/Factory.java
+++ src/main/java/edu/anadolu/Factory.java
@@ -1,10 +1,22 @@
 package edu.anadolu;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 class Factory {
 
     static String repo(DocType type) {
+
+        Properties properties = readProperties();
+        System.out.println(properties);
+
+        if (properties.getProperty(type.name()) != null)
+            return properties.getProperty(type.name());
+
         switch (type) {
             case Milliyet405bin:
                 return "/Users/iorixxx/Documents/MilliyetCollectionZipFiles";
@@ -41,5 +53,34 @@ class Factory {
             default:
                 throw new AssertionError(type);
         }
+    }
+
+    private static Properties readProperties() {
+
+        Properties prop = new Properties();
+
+        Path path = Paths.get("config.properties");
+
+        if (Files.exists(path) && Files.isReadable(path))
+
+            try (InputStream input = Files.newInputStream(Paths.get("config.properties"))) {
+                prop.load(input);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        else
+            try (InputStream input = Factory.class.getClassLoader().getResourceAsStream("config.properties")) {
+
+                if (input == null) {
+                    System.out.println("Sorry, unable to find config.properties in class path");
+                    return null;
+                }
+                prop.load(input);
+
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+        return prop;
     }
 }
