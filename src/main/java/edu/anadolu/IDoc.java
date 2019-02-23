c88a6e39f045908f5405d9d8bffaa43993441806
Ahmet Arslan <iorixxx@yahoo.com>
2019-02-24 02:32:51 +0300

    special chars are giving us a hard time

--- src/main/java/edu/anadolu/IDoc.java
+++ src/main/java/edu/anadolu/IDoc.java
@@ -16,9 +16,25 @@ public interface IDoc {
                 .replaceAll("´", "'")
 
                 .replaceAll("&rsquo;", "'")
+                .replaceAll("&lsquo;", "'")
                 .replaceAll("&quot;", "\"")
                 .replaceAll("&nbsp;", " ")
                 .replaceAll("&#226;", "a")
+
+                // https://www.degraeve.com/reference/specialcharacters.php
+                .replaceAll("&uuml;", "ü")
+                .replaceAll("&ccedil;", "ç")
+
+                .replaceAll("&ouml;", "ö")
+                .replaceAll("&oacute;", "o")
+                .replaceAll("&iacute;", "i")
+                .replaceAll("&uacute;", "u")
+                .replaceAll("&euml;", "e")
+                .replaceAll("&ecirc;", "e")
+                .replaceAll("&hellip;", "...")
+                .replaceAll("&atilde;", "a")
+                .replaceAll("&mdash;", "-")
+
 
                 .replaceAll("\\s+", " ")
                 .trim();
