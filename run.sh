c6a891a550b520e35c304134e9ae4941d5d36c52
Ahmet Arslan <iorixxx@yahoo.com>
2019-02-24 03:15:15 +0300

    add run script

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+mvn clean package
 java -server -Xms8g -Xmx8g -cp target/Kemik-1.0-SNAPSHOT.jar edu.anadolu.App
 java -server -Xms8g -Xmx8g -cp target/Kemik-1.0-SNAPSHOT.jar edu.anadolu.ListCompoundWords
 java -server -Xms8g -Xmx8g -cp target/Kemik-1.0-SNAPSHOT.jar edu.anadolu.Typo
